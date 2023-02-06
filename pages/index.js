bf326be17b0b2fe033a235cfece5fe57b07c2e14
newsprime <124551400+newsprime@users.noreply.github.com>
2023-02-06 11:21:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>5 Reasons why most people prefer to use news aggregator apps over watching news channels or reading newspapers. 
-</title>
+        <title>Next.js Toolbox</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -17,19 +16,10 @@ export default function Home() {
         <Header title="Next.js Toolbox" />
         <hr />
         <p className="description">
-          Many people nowadays prefer to use news aggregator apps on their mobile devices over watching TV news channels or reading newspapers when on their commutes, during leisure time, or while eating. People choose to read the complete news as soon as possible, choose their own category of news, or just glance at the trending headlines rather than reading the entire news bulletin, so the reasons vary based on the individual. 
+          Here's an example of a Netlify Form! When you fill this out, the
+          submissions can be found in the Netlify Admin site.
     
-    <ul>
-    
-    <li>Instead of reading the entire article, summarized news can be read & understood in just a few words. Reading the newspapers or seeing repeated telecasts of TV news channels is time-consuming, as you can read many summarized news in the same amount of time. 
-</li>
-    <li>Rather than scrolling through the whole list of unnecessary news headlines without knowing what you're looking for, you can choose the news category that interests you the most.
-</li>
-    <li>News aggregator news apps has the ability to save and share the news to anyone using social media channels or via messengers unlike newspapers and tv news channels. This day’s most of the tv news channels have their own websites and YouTube channels where these videos can also be shared via link. 
-</li>
-    
-    Live TV news channels offer a few benefits, but they require users to spend time and be in front of their televisions, which isn't possible if you're working or involved in any other activity. Users can stay up to date with the latest news by using news aggregators on the go. This is also why most of the television news channels have developed their own apps for deploying news and displaying it on the news channels as well. 
-
+    Many people nowadays prefer to use news aggregator apps on their mobile devices over watching TV news channels or reading newspapers when on their commutes, during leisure time, or while eating. People choose to read the complete news as soon as possible, choose their own category of news, or just glance at the trending headlines rather than reading the entire news bulletin, so the reasons vary based on the individual. 
 
         </p>
         <FeedbackForm />
